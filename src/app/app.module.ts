7c1eb16a5c962d8851fd68b3a60c2a36fcbb0a4b
Luca Corvino <l.corvino@live.it>
2023-04-13 01:39:01 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { ChronometerComponent } from './chronometer/chronometer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChronometerComponent,
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    FlexLayoutModule,
+    FontAwesomeModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
